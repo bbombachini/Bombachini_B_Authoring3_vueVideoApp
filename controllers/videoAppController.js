49c8813888f436eecac2ddb7f520e997b8c0088a
Barbara Bombachini <bbombachini@gmail.com>
2018-04-21 02:24:19 -0400

    Add select categories

--- controllers/videoAppController.js
+++ controllers/videoAppController.js
@@ -33,6 +33,31 @@ exports.get_all_movies = (req, res) => {
   });
 };
 
+exports.get_all_genres = (req, res) => {
+  connect.getConnection((err, connection) => {
+    connection.release();
+    if(err) {
+      return console.log(err.message);
+    }
+    if(connect.kids){
+       //Remove the genres crime and horror from kids view
+       var getGenre = `SELECT * FROM tbl_genre WHERE NOT (genre_id="4" OR genre_id="7")`;
+     } else {
+       var getGenre = `SELECT * FROM tbl_genre`;
+     }
+    connect.query(getGenre, (err, result) => {
+      if(err){
+        return console.log(err.message);
+      } else {
+        // console.log(result);
+        res.json({
+          genres: result
+        });
+      }
+    });
+  });
+};
+
 // connect.getConnection((err, connection) => {
 //   if(err) {
 //     return console.log(err.message);
